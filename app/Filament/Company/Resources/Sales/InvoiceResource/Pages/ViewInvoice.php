cc9baaea4b7258273f04165cfcfee25ce86b69c6
Andrew Wallo <andrewdwallo@gmail.com>
2024-12-08 21:41:24 -0500

    wip

--- app/Filament/Company/Resources/Sales/InvoiceResource/Pages/ViewInvoice.php
+++ app/Filament/Company/Resources/Sales/InvoiceResource/Pages/ViewInvoice.php
@@ -6,21 +6,31 @@ use App\Filament\Company\Resources\Sales\ClientResource;
 use App\Filament\Company\Resources\Sales\InvoiceResource;
 use App\Models\Accounting\Invoice;
 use Filament\Actions;
+use Filament\Infolists\Components\Grid;
 use Filament\Infolists\Components\Section;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\ViewEntry;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 use Filament\Support\Enums\FontWeight;
 use Filament\Support\Enums\IconPosition;
 use Filament\Support\Enums\IconSize;
+use Filament\Support\Enums\MaxWidth;
 
 class ViewInvoice extends ViewRecord
 {
+    protected static string $view = 'filament.company.resources.sales.invoices.pages.view-invoice';
+
     protected static string $resource = InvoiceResource::class;
 
     protected $listeners = [
         'refresh' => '$refresh',
     ];
+
+    public function getMaxContentWidth(): MaxWidth | string | null
+    {
+        return MaxWidth::SixExtraLarge;
+    }
 
     protected function getHeaderActions(): array
     {
@@ -49,39 +59,47 @@ class ViewInvoice extends ViewRecord
                 Section::make('Invoice Details')
                     ->columns(4)
                     ->schema([
-                        TextEntry::make('invoice_number')
-                            ->label('Invoice #'),
-                        TextEntry::make('status')
-                            ->badge(),
-                        TextEntry::make('client.name')
-                            ->label('Client')
-                            ->color('primary')
-                            ->weight(FontWeight::SemiBold)
-                            ->url(static fn (Invoice $record) => ClientResource::getUrl('edit', ['record' => $record->client_id])),
-                        TextEntry::make('total')
-                            ->label('Total')
-                            ->money(),
-                        TextEntry::make('amount_due')
-                            ->label('Amount Due')
-                            ->money(),
-                        TextEntry::make('date')
-                            ->label('Date')
-                            ->date(),
-                        TextEntry::make('due_date')
-                            ->label('Due')
-                            ->asRelativeDay(),
-                        TextEntry::make('approved_at')
-                            ->label('Approved At')
-                            ->placeholder('Not Approved')
-                            ->date(),
-                        TextEntry::make('last_sent')
-                            ->label('Last Sent')
-                            ->placeholder('Never')
-                            ->date(),
-                        TextEntry::make('paid_at')
-                            ->label('Paid At')
-                            ->placeholder('Not Paid')
-                            ->date(),
+                        Grid::make(1)
+                            ->schema([
+                                TextEntry::make('invoice_number')
+                                    ->label('Invoice #'),
+                                TextEntry::make('status')
+                                    ->badge(),
+                                TextEntry::make('client.name')
+                                    ->label('Client')
+                                    ->color('primary')
+                                    ->weight(FontWeight::SemiBold)
+                                    ->url(static fn (Invoice $record) => ClientResource::getUrl('edit', ['record' => $record->client_id])),
+                                TextEntry::make('amount_due')
+                                    ->label('Amount Due')
+                                    ->money(),
+                                TextEntry::make('due_date')
+                                    ->label('Due')
+                                    ->asRelativeDay(),
+                                TextEntry::make('approved_at')
+                                    ->label('Approved At')
+                                    ->placeholder('Not Approved')
+                                    ->date(),
+                                TextEntry::make('last_sent')
+                                    ->label('Last Sent')
+                                    ->placeholder('Never')
+                                    ->date(),
+                                TextEntry::make('paid_at')
+                                    ->label('Paid At')
+                                    ->placeholder('Not Paid')
+                                    ->date(),
+                            ])->columnSpan(1),
+                        Grid::make()
+                            ->schema([
+                                ViewEntry::make('invoice-view')
+                                    ->label('View Invoice')
+                                    ->columnSpan(3)
+                                    ->view('filament.company.resources.sales.invoices.components.invoice-view')
+                                    ->viewData([
+                                        'invoice' => $this->record,
+                                    ]),
+                            ])
+                            ->columnSpan(3),
                     ]),
             ]);
     }
